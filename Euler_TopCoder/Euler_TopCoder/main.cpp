c2f2d13369eec79c2fde62fe72b8835a90770768
edwardWang95 <edward.wang.95@gmail.com>
2016-08-17 19:53:09 -0400

    Setting up euler

---
+++ Euler_TopCoder/Euler_TopCoder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Euler_TopCoder
+//
+//  Created by Edward Wang on 8/17/16.
+//  Copyright © 2016 Edward Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
